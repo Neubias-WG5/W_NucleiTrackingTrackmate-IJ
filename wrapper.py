bd55208782a0b25890d45eb4941a35f6831053bf
SebastienTs <sebastien.tosi@gmail.com>
2020-01-14 16:37:37 +0100

    Added xvfb-run

--- wrapper.py
+++ wrapper.py
@@ -14,7 +14,7 @@ def main(argv):
 
         # 2. Call the image analysis workflow
         nj.job.update(progress=25, statusComment="Launching workflow...")
-        command = "ImageJ-linux64 --ij2 --headless --console --run " \
+        command = "/usr/bin/xvfb-run ./ImageJ-linux64 --ij2 --headless --console --run " \
 		   "/fiji/macros/Trackmate_script.py input={},output={},rad={},thr={},dst={},gapdst={},gap={},minlgth={}" \
 		   .format(in_path, out_path, nj.parameters.rad, nj.parameters.thr, nj.parameters.dst, nj.parameters.gapdst, nj.parameters.gap, nj.parameters.minlgth)	
         return_code = call(command, shell=True, cwd="/fiji")  # waits for the subprocess to return
